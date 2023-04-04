e586f49cfa3560af99a90b429123b9868409cbec
RobinDela <78260403+RobinDela@users.noreply.github.com>
2023-04-04 13:41:22 +0900

    Add japanese languages "About me"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,35 @@ import { BsFillPersonLinesFill } from 'react-icons/bs'
 import { Link } from 'react-scroll'
 import { LanguageContext } from '../LanguageContext';
 import languageData from '../data/languageData.json';
+import ResumeEng from '../assets/resume-eng-robin.pdf';
+import ResumeJp from '../assets/resume-jp-robin.pdf';
+
 
 
 const Navbar = () => {
+    const handleDownloadEng = () => {
+        const blob = new Blob([ResumeEng], { type: 'application/pdf' });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'resume-eng-robin.pdf';
+        link.click();
+    }
+    const handleDownloadJp = () => {
+        const blob = new Blob([ResumeJp], { type: 'application/pdf' });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'resume-jp-robin.pdf';
+        link.click();
+    }
+    const handleDownload = () => {
+        if (language === 'en') {
+            handleDownloadEng();
+        } else {
+            handleDownloadJp();
+        }
+    }
     const [nav, setNav] = useState(false)
     const handleClick = () => setNav(!nav)
 
@@ -118,6 +144,7 @@ const Navbar = () => {
                     </li>
                     <li className=' w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
                         <a
+                            onClick={handleDownload}
                             className='flex justify-between items-center w-full text-gray-300'
                             href='/' target="_blank"
                         >
